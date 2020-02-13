dbe31c1001227e19d598e77f800abe66fbe612c1
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-02-12 23:46:59 -0800

    Update selenium_scrape.py

--- Commute_Calculator/selenium_scrape.py
+++ Commute_Calculator/selenium_scrape.py
@@ -4,3 +4,5 @@ import pandas as pd
 # JESUS CHRIST make sure to include /chromedriver after temporary_paath
 webdriver = "/Users/levon/Documents/coding/temporary_paath/chromedriver"
 driver = Chrome(webdriver)
+
+URL = "https://www.google.com/maps/dir/Westaire+Engineering+Inc,+5820+Alameda+St,+Vernon,+CA+90058/Northridge,+Los+Angeles,+CA/"
