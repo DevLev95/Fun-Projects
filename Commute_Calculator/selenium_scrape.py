a457904521ac38ab1dad51aaae585112bb08f738
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-02-12 23:46:49 -0800

    Update selenium_scrape.py

--- Commute_Calculator/selenium_scrape.py
+++ Commute_Calculator/selenium_scrape.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Chrome
 import pandas as pd
 
+# JESUS CHRIST make sure to include /chromedriver after temporary_paath
 webdriver = "/Users/levon/Documents/coding/temporary_paath/chromedriver"
 driver = Chrome(webdriver)
