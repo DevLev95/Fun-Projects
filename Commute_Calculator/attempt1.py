41cda78f921a983b85666865ec3fdee40355c4fd
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-02-17 23:41:06 -0800

    Update attempt1.py

--- Commute_Calculator/attempt1.py
+++ Commute_Calculator/attempt1.py
@@ -8,3 +8,8 @@ with open('7.json', 'r') as myfile:
     data=myfile.read()
 # parse file
 obj = json.loads(data)
+
+# turned the json into a list that i can actually use
+# obj is a list in a dict in a list
+a = obj[0]
+cities = a["cities"]
